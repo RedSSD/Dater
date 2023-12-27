0dd4119c5d0e321583dd4e674c1b08387f2ace70
RedSSD <salatcaesarrr1@gmail.com>
2023-12-27 13:52:12 +0100

    alter token field

--- BackEnd/invitation/models.py
+++ BackEnd/invitation/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 from datetime import timedelta, date
+import hashlib
 
 
 class Invitation(models.Model):
     """
     Invitation Model
     """
+    def generate_token(self):
+        data = self.telegram_id.encode("utf-8")
+        return hashlib.sha256(data).hexdigest()
+
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30, blank=True, default='Someone')
     telegram_id = models.CharField(max_length=9, unique=True)
-    token = models.CharField(max_length=64, unique=True)
+    token = models.CharField(max_length=64, unique=True, default=generate_token)
     expiration_date = models.DateField(default=date.today() + timedelta(days=2))
+
+

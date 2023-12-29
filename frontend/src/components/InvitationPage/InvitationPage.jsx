b014dc689ff4c93e8c9b68dab5c440404a521421
RedSSD <salatcaesarrr1@gmail.com>
2023-12-29 02:43:33 +0100

    added gif and text changing after click

--- frontend/src/components/InvitationPage/InvitationPage.jsx
+++ frontend/src/components/InvitationPage/InvitationPage.jsx
@@ -1,24 +1,34 @@
 import css from './InvitationPage.module.css'
 import MovingButton from "./MovingButton";
+import {useState} from "react";
 
+
+const INVITATION_GIF_LINK = "https://media.giphy.com/media/Js27mGWPIUs9V0mjcd/giphy.gif"
+const RESULT_GIF_LINK = "https://media.giphy.com/media/j594eyQTt0CiRi9zI5/giphy.gif"
 
 function InvitationPage()
 {
 
+    const [gifLink, setGifLink] = useState(INVITATION_GIF_LINK);
+    const [text, setText] = useState("Do you wanna go out with me?");
 
+    const OnYesButtonClick = () => {
+        setGifLink(RESULT_GIF_LINK);
+        setText("YEAAAAHHHHH");
+    }
 
     return (
         <div className={css['invitation-container']}>
             <div>
-                <h1>Do you wanna go out with me?</h1>
+                <h1>{text}</h1>
             </div>
             <div className={css['gif-container']}>
                 <img
-                src= "https://media.giphy.com/media/Js27mGWPIUs9V0mjcd/giphy.gif"
+                src= {gifLink}
                 alt="Cute animated illustration"/>
             </div>
             <div className={css['buttons-section']}>
-                <button className={css['answer-button']}>Yes</button>
+                <button className={css['answer-button']} onClick={OnYesButtonClick}>Yes</button>
                 <MovingButton>No</MovingButton>
             </div>
         </div>

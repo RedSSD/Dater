19e466c1c857e3dff3ef5bf7f3fe860418e9621c
RedSSD <salatcaesarrr1@gmail.com>
2023-12-30 13:10:47 +0100

    added cron delete function

--- frontend/src/components/InvitationPage/MovingButton.jsx
+++ frontend/src/components/InvitationPage/MovingButton.jsx
@@ -1,7 +1,5 @@
-import {render} from "@testing-library/react";
 import {useRef, useState} from "react";
 import css from "./InvitationPage.module.css"
-
 
 const OFFSET_WIDTH = 19;
 const OFFSET_HEIGHT = 66;
@@ -22,10 +20,11 @@ function MovingButton() {
             <button
                 className={css["answer-button"]}
                 onMouseOver={OnMouseOver}
-                style={isHover ? { position: 'absolute', left: position.left, top: position.top } : null}
+                style={isHover ?
+                    { position: 'absolute', left: position.left, top: position.top } : null
+                }
             >No</button>
         )
-
 }
 
 export default MovingButton;

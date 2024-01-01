5031deb987ef8ea85462ed3b9234b3e0c94e0c7f
RedSSD <salatcaesarrr1@gmail.com>
2024-01-01 12:38:44 +0100

    changed model

--- BackEnd/invitation/models.py
+++ BackEnd/invitation/models.py
@@ -7,13 +7,21 @@ class Invitation(models.Model):
     """
     Invitation Model
     """
-    def generate_token(self):
-        data = self.telegram_id.encode("utf-8")
-        return hashlib.sha256(data).hexdigest()
 
     id = models.AutoField(primary_key=True)
     telegram_id = models.CharField(max_length=9, unique=True)
-    token = models.CharField(max_length=64, unique=True, default=generate_token)
+    token = models.CharField(max_length=64, unique=True, blank=True, null=True)
     expiration_date = models.DateField(default=date.today() + timedelta(days=2))
+
+    def save(self, *args, **kwargs):
+        self.token = self.generate_token()
+        super().save(*args, **kwargs)
+
+    def generate_token(self):
+        data = str(self.telegram_id).encode("utf-8")
+        return hashlib.sha256(data).hexdigest()
+
+    def __str__(self):
+        return f'{self.telegram_id}'
 
 

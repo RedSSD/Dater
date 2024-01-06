36b700cd29bbefc824c80b166656bd7261a460d9
RedSSD <salatcaesarrr1@gmail.com>
2024-01-06 13:00:09 +0100

    connected invitaion page with api

--- frontend/src/components/InvitationPage/InvitationPage.jsx
+++ frontend/src/components/InvitationPage/InvitationPage.jsx
@@ -2,11 +2,11 @@ import css from './InvitationPage.module.css'
 import MovingButton from "../UI/MovingButton";
 import {useState} from "react";
 import Button from "../UI/Button";
-import {useParams} from "react-router-dom";
+import axios from "axios";
 
 
-const INVITATION_GIF_LINK = "https://media.giphy.com/media/Js27mGWPIUs9V0mjcd/giphy.gif"
-const RESULT_GIF_LINK = "https://media.giphy.com/media/j594eyQTt0CiRi9zI5/giphy.gif"
+const INVITATION_GIF_LINK = ""//"https://media.giphy.com/media/Js27mGWPIUs9V0mjcd/giphy.gif"
+const RESULT_GIF_LINK = ""//"https://media.giphy.com/media/j594eyQTt0CiRi9zI5/giphy.gif"
 const SUCCESS_TEXT = "YEAAAAHHHHH"
 
 function InvitationPage()
@@ -15,15 +15,17 @@ function InvitationPage()
     const [gifLink, setGifLink] = useState(INVITATION_GIF_LINK);
     const [text, setText] = useState("Do you wanna go out with me?");
 
-    const urlParams = useParams();
-
-    const checkIsTokenValid = () => {
-        // TODO send request and get tg_id or "idi nahui zaebal"
-    }
-
     const OnYesButtonClick = () => {
-        setGifLink(RESULT_GIF_LINK);
-        setText(SUCCESS_TEXT);
+        const token = window.location.pathname
+        const url = process.env.REACT_APP_BASE_API_URL + `${token}`
+
+
+        axios.get(url).then(res =>{
+            if(res.status === 200) {
+                setGifLink(RESULT_GIF_LINK);
+                setText(SUCCESS_TEXT);
+            }
+        })
     }
 
     return (

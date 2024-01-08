7dc5e6abfe8415278073fe879d75755204e2d69e
Illia Tarasenko <99745038+RedSSD@users.noreply.github.com>
2024-01-08 13:36:30 +0100

    removed domains from CORS whitelist

--- BackEnd/BackEnd/settings.py
+++ BackEnd/BackEnd/settings.py
@@ -68,10 +68,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ORIGIN_WHITELIST = [
-    "http://localhost:8080",
     "http://localhost:3000",
-    "http://127.0.0.1",
-    "http://0.0.0.0",
 ]
 
 CORS_ALLOWED_ORIGINS = [
